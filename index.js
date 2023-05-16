c8dab7bff670426e1e2ae067c2f35bfe9ecd5471
Kendall Malik Howard <kendallhoward04@gmail.com>
2023-05-16 16:40:09 -0700

    Added Updates

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-require('dotenv').config()
-const express = require('express')
-const book = express()
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
 
 
-book.use('/', (req, res) => {
-    res.send('Hello World')
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+   () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+);
+
+app.get('/', (req, res) => {
+   res.send('Hello World')
+});
+
+const booksController = require('./controller/books_controller.js');
+app.use('/books', booksController);
+
+
+app.listen(PORT, () => {
+    console.log(`listening on port: ${PORT}`)
 })
-
-book.get('/', (req, res) => {
-    res.send('GET/books')
-})
-
-
-book.listen(process.env.PORT)
--- models/books.js
+++ models/books.js
@@ -1,11 +1,13 @@
-module.exports = [{
-    
-        "id": 1,
-        "title": "The Shinobi Initiative",
-        "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
-        "year": 2014,
-        "quantity": "10",
-        "imageURL": "/assets/shinobi-initiative.jpeg"
-      
-    
-}]
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const bookSchema = new Schema({
+    title: String,
+    description: String,
+    year: Number,
+    quantity: Number,
+    imageURL: String,
+});
+
+const Book = mongoose.model( 'Book', bookSchema);
+module.exports = Book
